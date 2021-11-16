4c08c25b455021a30f7306bca28a3a8f84bcc998
Giovi-17 <giovanni.nuzzo17@gmail.com>
2021-11-16 15:54:34 +0100

    Milestone 3(Traccia)

--- MILESTONE 3/js/script.js
+++ MILESTONE 3/js/script.js
@@ -1,5 +1,7 @@
-/* Dato un container nel DOM, 
-appendi un elemento html con il numero o la stringa corretta. */
+/* Applica uno stile differente a seconda del valore dell'indice 
+per i multipli di 3, 
+per i multipli di 5 
+e per i valori che sono sia multipli di 3 che di 5. */
 
 const containerTotal = document.querySelector('.container');
 
